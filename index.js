ea1a3d3ecef9aa2a55b056c43a09e9ec785ebdea
Bawer <bawermy123@gmail.com>
2024-09-13 15:08:37 +0200

    mobile layout + errors

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const monthInput = document.getElementById('month')
 const yearInput = document.getElementById('year')
 
 dayInput.addEventListener('change', event => {
-    if (Number(dayInput.value) > 31) {
-        dayInput.value = 31
-    }
+    dayInput.parentElement.classList.remove("required-error")
+    dayInput.parentElement.classList.remove("invalid-error")
+    // if (Number(dayInput.value) > 31) {
+    //     dayInput.value = 31
+    // }
+    if (["", "0", "00"].includes(monthInput.value)) return
     dayInput.value = Number(dayInput.value)
     if (String(dayInput.value).length === 1) {
         dayInput.value = "0" + dayInput.value
@@ -13,9 +16,12 @@ dayInput.addEventListener('change', event => {
 })
 
 monthInput.addEventListener('change', event => {
-    if (Number(monthInput.value) > 12) {
-        dayInput.value = 12
-    }
+    monthInput.parentElement.classList.remove("required-error")
+    monthInput.parentElement.classList.remove("invalid-error")
+    // if (Number(monthInput.value) > 12) {
+    //     dayInput.value = 12
+    // }
+    if (["", "0", "00"].includes(monthInput.value)) return
     monthInput.value = Number(monthInput.value)
     if (String(monthInput.value).length === 1) {
         monthInput.value = "0" + monthInput.value
@@ -23,12 +29,47 @@ monthInput.addEventListener('change', event => {
 })
 
 yearInput.addEventListener('change', event => {
-    yearInput.value = Number(yearInput.value)
+    yearInput.parentElement.classList.remove("required-error")
+    yearInput.parentElement.classList.remove("invalid-error")
+    if (monthInput.value === "") return
+    monthInput.value = Number(monthInput.value)
 })
+
 
 const button = document.getElementById('button')
 
 button.addEventListener('click', event => {
+    let returning = false
+    if (dayInput.value === "") {
+        dayInput.parentElement.classList.add('required-error')
+        returning = true
+    }
+    if (monthInput.value === "") {
+        monthInput.parentElement.classList.add('required-error')
+        returning = true
+    }
+    if (yearInput.value === "") {
+        yearInput.parentElement.classList.add('required-error')
+        returning = true
+    }
+    if ((1 > Number(monthInput.value) || Number(monthInput.value) > 12) && monthInput.value !== "") {
+        returning = true
+        monthInput.parentElement.classList.add('invalid-error')
+        if ((1 > Number(dayInput.value) || Number(dayInput.value) > 31) && dayInput.value !== "") dayInput.parentElement.classList.add('invalid-error')
+    } else if ((1 > Number(dayInput.value) || Number(dayInput.value) > new Date(yearInput.value, monthInput.value, 0).getDate()) && dayInput.value !== "") {
+        dayInput.parentElement.classList.add('invalid-error')
+        returning = true
+    }
+    if (yearInput.value > new Date().getFullYear()) {
+        yearInput.parentElement.classList.add('invalid-error')
+        returning = true
+    }
+    if (returning) {
+        document.getElementById('output-days').innerText = "--"
+        document.getElementById('output-months').innerText = "--"
+        document.getElementById('output-years').innerText = "--"
+        return
+    }
     const currentDate = new Date()
     let day = currentDate.getDate() - Number(dayInput.value)
     let month = currentDate.getMonth() - Number(monthInput.value) + 1
